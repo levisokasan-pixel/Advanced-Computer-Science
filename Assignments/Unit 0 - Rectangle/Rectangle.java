851baea5fd9e49733322eea9c0bd144bb83ef182
levisokasan-pixel <levisokasan@gmail.com>
2025-09-11 13:13:14 -0700

    Finished Rectangle Lab

--- Assignments/Unit 0 - Rectangle/Rectangle.java
+++ Assignments/Unit 0 - Rectangle/Rectangle.java
@@ -7,42 +7,47 @@ public class Rectangle {
         length = inputLength;
         width = inputWidth;
     }
-    
+
     public Rectangle() {
-        int length = 1;
-        int width = 4;
+        length = 6;
+        width = 7;
     }
 
     public int getLength() {
         return length;
     }
-    
+
     public int getWidth() {
         return width;
     }
 
-    public void setLength(int input) {
-        length = input;
+    public void setLength(int changedLength) {
+        length = changedLength;
     }
 
-    public void setWidth(int input) {
-        width = input;
+    public void setWdith(int changedWidth) {
+        length = changedWidth;
     }
 
-    public String toString(int length, int width) {
-        return "This rectangle has a length of " + length + " and a width of " + width + ". It's area is " + length * width + ".";
+    public String toString() {
+        return "has a length of " + length + " and a width of " 
+            + width + ". It's area is " + length * width + ".";
     }
 
-    public int calculateArea(int length, int width) {
+    public int calculateArea() {
         return length * width;
     }
 
-    // public boolean equals(Rectangle) {
+    public boolean equals(Rectangle other) {
+        return length == other.length && width == other.width; 
+    }
 
-    // }
-
-    public int calculatePerimeter(int length, int width) {
+    public int calculatePerimeter() {
         return 2 * length + 2 * width;
+    }
+
+    public double calculateDiagonal() {
+        return Math.sqrt(length * length + width * width);
     }
 
 }
--- Assignments/Unit 0 - Rectangle/RectangleTester.java
+++ Assignments/Unit 0 - Rectangle/RectangleTester.java
@@ -1,11 +1,38 @@
 public class RectangleTester {
     public static void main(String[] args) {
         
+        System.out.println("--------------------");
         Rectangle rectangle = new Rectangle(4, 5);
-
-        int rectangleLength = rectangle.getLength();
-        int rectangleWidth = rectangle.getWidth();
-
+        Rectangle otherRectangle = new Rectangle();
+        System.out.println("Rectangle 1 " + rectangle.toString());
+        System.out.println("Rectangle 2 " + otherRectangle.toString());
+        System.out.println("--------------------");
+        System.out.println("The perimeter of Rectangle 1 is " 
+            + rectangle.calculatePerimeter() + ".");
+        System.out.println("The perimeter of Rectangle 2 is " 
+            + otherRectangle.calculatePerimeter() + ".");
+        System.out.println("--------------------");
+        System.out.println("The diagonal length of Rectangle 1 is " 
+            + rectangle.calculateDiagonal());
+        System.out.println("The diagonal length of Rectangle 2 is " 
+            + otherRectangle.calculateDiagonal());
+        System.out.println("--------------------");
+        System.out.println("The fact that these two rectangles equal each other is " 
+            + rectangle.equals(otherRectangle) + ".");
+        System.out.println("--------------------");
+        rectangle.setLength(2);
+        rectangle.setWdith(2);
+        System.out.println("Rectangle 1 now " + rectangle.toString());
+        System.out.println("--------------------");
+        System.out.println("The fact that these two rectangles equal each other is " 
+            + rectangle.equals(otherRectangle) + ".");
+        System.out.println("--------------------");
+        System.out.println("The perimeter of Rectangle 1 is now " 
+            + rectangle.calculatePerimeter() + ".");
+        System.out.println("--------------------");
+        System.out.println("The diagonal length of Rectangle 1 is now " 
+            + rectangle.calculateDiagonal());
+        System.out.println("--------------------");
 
     }
 }
